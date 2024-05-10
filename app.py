7e9ce4270a97ee1fe8dc5102fcaaffa3eb573736
ReplacedSpace17 <replacedspace17@gmail.com>
2024-05-09 19:04:58 -0600

    Actualiza configuración de servidor en app.py

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 import sys, os
 from typing import List
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
 import databseConn
 
 from mqtt_suscriber import TopicItem, subscribe_topics
+
+from localStoragePy import localStoragePy
 
 
 app = FastAPI()
 
 subscription_active = False
+
+localStorage = localStoragePy('my_app_namespace', 'json')
+
 
 @app.post("/subscribe/")
 async def handle_subscription(topics: List[TopicItem]):
@@ -31,6 +36,32 @@ def ping():
     print("Ping")
     return {"message": "Servidor en línea"}
 
+
+@app.post("/MqttConfig")
+async def CreateConfig(request: Request):
+    body = await request.json()
+    #SET ITEM FROM BODY
+    localStorage.setItem('mqtt_broker', body["mqtt_broker"])
+    localStorage.setItem('mqtt_port', body["mqtt_port"])
+    localStorage.setItem('mysql_host', body["mysql_host"])
+    localStorage.setItem('mysql_user', body["mysql_user"])
+    localStorage.setItem('mysql_password', body["mysql_password"])
+    localStorage.setItem('mysql_db', body["mysql_db"])
+    localStorage.setItem('mqtt_topics', body["mqtt_topics"])
+    print("Configuracion recibida")
+    return {"message": "Configuracion recibida", "data": body}
+
+@app.get("/MqttConfig")
+def GetConfig():
+    mqtt_broker = localStorage.getItem('mqtt_broker')
+    mqtt_port = localStorage.getItem('mqtt_port')
+    mysql_host = localStorage.getItem('mysql_host')
+    mysql_user = localStorage.getItem('mysql_user')
+    mysql_password = localStorage.getItem('mysql_password')
+    mysql_db = localStorage.getItem('mysql_db')
+    mqtt_topics = localStorage.getItem('mqtt_topics')
+    return {"message": "Se obtuvo la configuracion", "data": {"mqtt_broker": mqtt_broker, "mqtt_port": mqtt_port, "mysql_host": mysql_host, "mysql_user": mysql_user, "mysql_password": mysql_password, "mysql_db": mysql_db, "mqtt_topics": mqtt_topics}}
+
     
 if __name__ == "__main__":
     import uvicorn
@@ -39,4 +70,4 @@ if __name__ == "__main__":
     #databseConn.verificar_conexion()
     #databseConn.verificar_conexion_internet()
     print("Esperando mensajes MQTT...")
-    uvicorn.run("app:__main__", host= "0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
